1c41b4c8478ebdfc3d828e2bc58aca2666c0431e
Robert Tate <btate712@gmail.com>
2019-03-18 11:50:07 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,16 @@
 class Course
   attr_accessor :title, :schedule, :description
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+  
+  def reset_all
+    @@all = []
+  end
+
+  def self.all
+    @@all
+  end
 end
